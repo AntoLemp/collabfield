4df18983acb5d1f9b3a5a9c5911869cef454237f
AntoLemp <160651031+AntoLemp@users.noreply.github.com>
2026-02-15 12:56:00 +0200

    Add user name field to sign up and profile update forms; enhance styles and structure

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
