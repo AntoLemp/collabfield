f9f2f9aad0102bb49e1e9385da781ed7b56363bf
AntoLemp <160651031+AntoLemp@users.noreply.github.com>
2026-02-15 22:21:36 +0200

    Add posts index view and styling; implement posts controller and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
